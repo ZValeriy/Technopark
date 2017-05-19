1e3ba28964035b8767724219e2b7e7a4c310ab34
ZubairovValeriy <ValeriyIU5-12@yandex.ru>
2017-05-20 01:35:05 +0300

    Add files via upload

---
+++ WebTP/6.cpp
@@ -0,0 +1,44 @@
+﻿//Дано N кубиков.Требуется определить каким количеством способов
+//можно выстроить из этих кубиков пирамиду.
+//Формат входных данных :
+//На вход подается количество кубиков N.
+//Формат выходных данных :
+//Вывести число различных пирамид из N кубиков.
+//6_2. Широкая пирамида.​ Каждый вышележащий слой 
+//пирамиды должен быть строго меньше
+//нижележащего.
+//N ≤ 300.
+
+#include<iostream>
+#include<cassert>
+
+int Calculating(int numbers)
+{
+	int piramides = 0;                       //Количество пирамид
+	int downrow = 0;                         //Количество кубиков в нижнем ряду
+	if (numbers <= 2) return 1;             
+	if (numbers % 2 == 0)
+	{
+		downrow = numbers / 2;
+	}
+	else downrow = numbers / 2 + 1;
+	for (int i = 0; i < downrow; i++) {
+		piramides+=Calculating(i);
+	}
+	return piramides;
+}
+
+
+
+
+int main()
+{
+	int N = 0;
+	std::cin >> N;
+	assert(N <= 300 && N > 0);
+
+	int count = Calculating(N);
+	std::cout << count;
+
+	return 0;
+}
